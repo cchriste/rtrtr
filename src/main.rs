f945241af20bfd08c36ac74f73dea2667d4d4b02
Cameron Christensen <cam@sci.utah.edu>
2021-09-11 18:50:41 -0600

    a few better functions in vec; improved setup and use of canvas; seemingly correct ray_color function, but color doesn't seem bright enough

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f32 / ASPECT) as u32;
 // - global variables? (`let foo = 42;` doesn't work)
 //   let mut rng = rand::thread_rng(); // doesn't work
 // - multiple files, please!
+// - ternary expression guaranteeing assignment: let a = if x > 5 { 10 } else { 7 };
 
 #[derive(Debug)]  // enables it to be printed. What else?
 #[derive(Copy, Clone)]
@@ -45,12 +46,13 @@ impl Vector {
     fn init(x: f32, y: f32, z: f32) -> Vector {
         Vector { v: [x, y, z] }
     }
-
-    fn negate(&mut self) -> &Vector {
-        self.v[0] = -self.v[0];
-        self.v[1] = -self.v[1];
-        self.v[2] = -self.v[2];
-        self
+    fn x(&self) -> f32 { self.v[0] }
+    fn y(&self) -> f32 { self.v[1] }
+    fn z(&self) -> f32 { self.v[2] }
+    fn len(&self) -> f32 {
+        (self.v[0]*self.v[0] +
+         self.v[1]*self.v[1] +
+         self.v[2]*self.v[2]).sqrt()
     }
     fn dot(&self, other: &Vector) -> Vector {
         Vector { v: [self.v[0] * other.v[0],
@@ -62,11 +64,20 @@ impl Vector {
                      self.v[1] + other.v[1],
                      self.v[2] + other.v[2]] }
     }
-    // todo: +=, etc
+    fn sub(&self, other: &Vector) -> Vector {
+        Vector { v: [self.v[0] - other.v[0],
+                     self.v[1] - other.v[1],
+                     self.v[2] - other.v[2]] }
+    }
     fn mul(&self, k: f32) -> Vector {
         Vector { v: [self.v[0] * k,
                      self.v[1] * k,
                      self.v[2] * k] }
+    }
+    fn div(&self, k: f32) -> Vector {
+        Vector { v: [self.v[0] / k,
+                     self.v[1] / k,
+                     self.v[2] / k] }
     }
     fn cross(&self, other: &Vector) -> Vector {
         //        |  î   ĵ   k̂ |
@@ -79,16 +90,20 @@ impl Vector {
                      self.v[2]*other.v[0] - self.v[0]*other.v[2],
                      self.v[0]*other.v[1] - self.v[1]*other.v[0]] }
     }
+    // <ctc> is this really necessary? I don't think so.
+    fn negate(&mut self) -> &Vector {
+        self.v[0] = -self.v[0];
+        self.v[1] = -self.v[1];
+        self.v[2] = -self.v[2];
+        self
+    }
     fn normalize(&mut self) -> &Vector {
-        let magnitude = (self.v[0]*self.v[0] +
-                         self.v[1]*self.v[1] +
-                         self.v[2]*self.v[2]).sqrt();
+        let magnitude = self.len();
         self.v[0] /= magnitude;
         self.v[1] /= magnitude;
         self.v[2] /= magnitude;
         self
     }
-
     fn normalized(mut vec: Vector) -> Vector {
         vec.normalize();
         vec
@@ -96,21 +111,27 @@ impl Vector {
 }
 
 // todo: read ch 4 on ownership after unsuccessfully trying all sorts of variations here
+// still curious since it's a function that modifies an input -> ** just don't return **
 // fn dotself(&(mut vec): &[f32; 3]) -> &[f32; 3] {
 //     vec[0] *= vec[0];
 //     vec[1] *= vec[1];
 //     vec[2] *= vec[2];
 //     return &vec; // doesn't work (neither does just &vec or vec
 // }
+
 // todo class ray: (origin: [f32; 3], dir: [f32; 3]);
 
+// color of ray(origin, dir)
 fn ray_color(ray: &(Vector, Vector)) -> Vector {
-    // let (_origin, dir) = ray;
-    // let dir = dir.normalize(dir);// hmm... can I get a _copy_ of ray.dir?
-    let dir = Vector::normalized(ray.1);
-    Vector { v: [0.5 * (dir.v[1] + 1.0),
-                 rand::random(),
-                 0.5 * (dir.v[0] + 1.0)] }
+    let unit_dir = Vector::normalized(ray.1);
+    if (1.0 - unit_dir.len()).abs() > f32::EPSILON {
+        println!("unit dir len isn't correct! {:?}, {:?}", unit_dir, ray.1);
+    }
+    let t = 0.5*(unit_dir.y() + 1.0); // vertical percent along viewport
+    let white = Vector::init(1.0, 1.0, 1.0);
+    let bluey = Vector::init(0.5, 0.7, 1.0);
+    //white.mul(1.0 - t).add(&bluey.mul(t))
+    bluey.mul(t)
 }
 
 // try to reduce annoyingly verbose array indices from u32s
@@ -119,15 +140,12 @@ fn ray_color(ray: &(Vector, Vector)) -> Vector {
 
 fn main() {
 
-    // are there operators? can I make my own?
-    let mut foo = 42;
+/*
+    // experimental learning stuff (but annoying output)
+
     let str = "create a png";
     // TODO: how do I print the type of str?
     println!("{} {}", str, foo); // note the ! after println cuz it's a macro
-    foo += 42;
-    //init("bowwwah!" + str(foo)); // TODO: figure out how to combine string and stuff
-    init("bowwwah!"); // TODO: figure out how to combine string and stuff
-    println!("start with the answer: {}", foo);
 
     let v1 = Vector { v: [1.0, 2., 3.] };
     let v2 = Vector { v: [4., -1., 3.] };
@@ -140,68 +158,73 @@ fn main() {
     println!("vec: {:?}", vec);
     vec.negate();
     println!("vec after negation: {:?}", vec);
+*/
+
+    // ////////////////////////////////////////// //
+
 
     // viewport
     const VIEWPORT_HEIGHT: f32 = 2.0;
     const VIEWPORT_WIDTH: f32 = VIEWPORT_HEIGHT * ASPECT;
-    const FOCAL_LENGTH: f32 = 1.0;
+    const FOCAL_LENGTH: f32 = 0.25;//1.0;
 
     let origin = Vector::init(0.0, 0.0, 0.0);
-    let right = Vector::init(1.0, 0.0, 0.0);
-    let up = Vector::init(0.0, 1.0, 0.0);
-    // let right = Vector::init(-1.0, 0.0, 0.0); // rot -180
-    // let up = Vector::init(0.0, -1.0, 0.0);
+    let right = Vector::init(VIEWPORT_WIDTH, 0.0, 0.0);
+    let up = Vector::init(0.0, VIEWPORT_HEIGHT, 0.0);
     let z = right.cross(&up);
+    let look = z.mul(-1.0);
 
-    //let topleft = [origin - right/2.0, origin + up/2.0, origin[2] - FOCAL_LENGTH];
-    let mut topleft = origin;
-    topleft = topleft.add(&z.mul(-FOCAL_LENGTH));
-    topleft = topleft.add(&right.mul(-0.5));
-    topleft = topleft.add(&up.mul(0.5));
-    println!("topleft: {:?}", topleft);
+    //let botleft = origin - right/2.0 - up/2.0 + look*FOCAL_LENGTH];
+    let botleft = origin.sub(&right.mul(0.5)).sub(&up.mul(0.5)).add(&look.mul(FOCAL_LENGTH));
+    println!("botleft: {:?}", botleft);
 
-    // image
+    // allocate image (just set length, don't initialize)
     let mut img: Vec<f32> = Vec::with_capacity(usize::try_from(4 * IMAGE_WIDTH * IMAGE_HEIGHT).unwrap());
     unsafe { img.set_len(img.capacity()); }
 
+    // keep track of min/max color values
     let mut color_bounds: ([f32; 3], [f32; 3]) = ([1.0, 1.0, 1.0], [0.0, 0.0, 0.0]);
 
-    for i in 0..IMAGE_HEIGHT {
-        for j in 0..IMAGE_WIDTH {
-            // let ray: vec = (origin,
-            //                topleft +
-            //                i / IMAGE_WIDTH * VIEWPORT_WIDTH +
-            //                j / IMAGE_HEIGHT * VIEWPORT_HEIGHT - origin);
-            let ray: (Vector, Vector) =
-                (origin,
-                 topleft.add(&right.mul(i as f32 / IMAGE_WIDTH as f32 * VIEWPORT_WIDTH))
-                 .add(&up.mul(-1.0 * i as f32 / IMAGE_HEIGHT as f32 * VIEWPORT_HEIGHT)));
+    for j in 0..IMAGE_HEIGHT {
+        for i in 0..IMAGE_WIDTH {
+            let pct_x = i as f32 / (IMAGE_WIDTH-1) as f32;
+            let pct_y = j as f32 / (IMAGE_HEIGHT-1) as f32;
 
-            let c = ray_color(&ray);
+            // ray: origin = O, direction = point moving across image from botleft - origin
+            let p1 = botleft.add(&right.mul(pct_x)).add(&up.mul(pct_y));
+            let p0 = origin;
+            let ray = (origin, p1.sub(&p0));
+
+            let color = ray_color(&ray);
             // if j % IMAGE_WIDTH == 0 {
-            //     println!("color: {:?}", c);
+            //     println!("color: {:?}", color);
             // }
 
             // update color minmax
-            for i in 0..3 {
-                color_bounds.0[i] = color_bounds.0[i].min(c.v[i]);
-                color_bounds.1[i] = color_bounds.1[i].max(c.v[i]);
+            for c in 0..3 {
+                color_bounds.0[c] = color_bounds.0[c].min(color.v[c]);
+                color_bounds.1[c] = color_bounds.1[c].max(color.v[c]);
             }
 
-            img[usize::try_from(4*(i * IMAGE_WIDTH + j) + 0).unwrap()] = c.v[0];
-            img[usize::try_from(4*(i * IMAGE_WIDTH + j) + 1).unwrap()] = c.v[1];
-            img[usize::try_from(4*(i * IMAGE_WIDTH + j) + 2).unwrap()] = c.v[2];
-            img[usize::try_from(4*(i * IMAGE_WIDTH + j) + 3).unwrap()] = 1.0;
+            img[usize::try_from(4*(j * IMAGE_WIDTH + i) + 0).unwrap()] = color.v[0];
+            img[usize::try_from(4*(j * IMAGE_WIDTH + i) + 1).unwrap()] = color.v[1];
+            img[usize::try_from(4*(j * IMAGE_WIDTH + i) + 2).unwrap()] = color.v[2];
+            img[usize::try_from(4*(j * IMAGE_WIDTH + i) + 3).unwrap()] = 1.0;
         }
     }
 
     // compute color range
-    // TODO: x-x = NaN; fixme
-    let c_rng = Vector::init(1.0/(color_bounds.1[0] - color_bounds.0[0]),
-                             1.0/(color_bounds.1[1] - color_bounds.0[1]),
-                             1.0/(color_bounds.1[2] - color_bounds.0[2]));
+    // TODO: x-x = NaN or inf; fixme
+    let rrng = 1.0/(color_bounds.1[0] - color_bounds.0[0]);
+    let grng = 1.0/(color_bounds.1[1] - color_bounds.0[1]);
+    let brng = 1.0/(color_bounds.1[2] - color_bounds.0[2]);
+    let c_rng = Vector::init(if rrng.is_normal() { rrng } else { 1.0 },
+                             if grng.is_normal() { grng } else { 1.0 },
+                             if brng.is_normal() { brng } else { 1.0 });
     println!("color_bounds: {:#?}", color_bounds);
     println!("color_range_scalar: {:#?}", c_rng);
+
+    //let c_rng = Vector::init(1.0, 1.0, 1.0);  // remove scaling (don't really want it actually)
 
     // scale color to [0..1]
     for i in 0..IMAGE_HEIGHT*IMAGE_WIDTH {
@@ -210,25 +233,17 @@ fn main() {
         let b = img[usize::try_from(4*i + 2).unwrap()];
         let a = img[usize::try_from(4*i + 3).unwrap()];
 
-        img[usize::try_from(4*i + 0).unwrap()] = (r - color_bounds.0[0]) * c_rng.v[0];
-        img[usize::try_from(4*i + 1).unwrap()] = (g - color_bounds.0[1]) * c_rng.v[1];
-        img[usize::try_from(4*i + 2).unwrap()] = (b - color_bounds.0[2]) * c_rng.v[2];
+        img[usize::try_from(4*i + 0).unwrap()] = (r - color_bounds.0[0]) * c_rng.v[0] + color_bounds.0[0];
+        img[usize::try_from(4*i + 1).unwrap()] = (g - color_bounds.0[1]) * c_rng.v[1] + color_bounds.0[1];
+        img[usize::try_from(4*i + 2).unwrap()] = (b - color_bounds.0[2]) * c_rng.v[2] + color_bounds.0[2];
+        // img[usize::try_from(4*i + 0).unwrap()] = r;
+        // img[usize::try_from(4*i + 1).unwrap()] = g;
+        // img[usize::try_from(4*i + 2).unwrap()] = b;
         img[usize::try_from(4*i + 3).unwrap()] = a;
     }
 
-
-    write_img(r"/tmp/scaled_canvas.png", img);
+    write_img(r"/tmp/canvas_color.png", img);
     conclude("Goodbye fellow Rustaceans!");
-}
-
-fn init(arg: &str) -> u32{
-    println!("welcome to fn!");
-    println!("arg: {}", arg);
-
-    for i in 0..3 {
-        println!("{}", i);
-    }
-    return 42;
 }
 
 fn conclude(msg: &str) {
